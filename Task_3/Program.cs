4940b1b2c48aace5f1cc3cde919bf3b681655fd9
Savodyarkin Nikita <snikitafut@mail.ru>
2023-12-03 16:10:45 +0300

    difference between max and min

---
+++ Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+double [] array = {3.14, 2.718, 6.66, 9.99, 4.56};
+
+double max = array[0];
+double min = array[0];
+
+for (int i = 1; i < array.Length; i++) {
+    if (array[i] > max) {
+        max = array[i];
+    } else if (array[i] < min) {
+        min = array[i];
+    }
+}
+
+Console.WriteLine($"Максимальное значение: {max}");
+Console.WriteLine($"Минимальное значение: {min}");
+Console.WriteLine($"Разница элементов: {max - min}");
